c2ca27e8a8a71828da5fa17274d695fc502012a7
root <alex.d.brant@gmail.com>
2018-06-21 01:16:09 +0000

    sampletwig packaging

---
+++ pkg/create_install_package.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh
+echo create install package for $1
+
+INSTALL_DIR=$1
+cd  ../orac/modules/
+if [ -f "$INSTALL_DIR/deploy.sh" ]
+then
+    chmod +x "$INSTALL_DIR/deploy.sh"
+fi
+
+if [ -f "$INSTALL_DIR/manifest.txt" ]
+then
+    echo move existing manifest away
+    cp "$INSTALL_DIR/manifest.txt" /tmp
+    rm "$INSTALL_DIR/manifest.txt"
+fi
+rm -rf __MACOSX
+
+find "$INSTALL_DIR" -type f ! -name "._*" -print0  | xargs -0 sha1sum > /tmp/manifest.new
+cp /tmp/manifest.new "$INSTALL_DIR/manifest.txt"
+rm /tmp/manifest.new
+pwd
+
+zip -r "$1.zop" "$INSTALL_DIR"/*
+
+mv "$1.zop" ../../pkg
