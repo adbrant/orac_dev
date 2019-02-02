4fe4cacb73d4729ddee830c758e146fd765935c6
adbrant <alex.d.brant@gmail.com>
2019-02-02 13:27:09 -0500

    Update run.sh

--- gitpull/run.sh
+++ gitpull/run.sh
@@ -6,15 +6,25 @@ oscsend localhost 4001 /oled/setscreen i 1
 
 oscsend localhost 4001 /oled/aux/line/2 s "Pulling"
 oscsend localhost 4001 /oled/aux/line/3 s "Updates"
-
+changed=0
 cd ..
-git pull
+git pull | grep -q -v 'Already up-to-date.' && changed=1
 sleep 1
-
 
 oscsend localhost 4001 /oled/aux/clear i 1
 oscsend localhost 4001 /oled/setscreen i 1
 
-oscsend localhost 4001 /oled/aux/line/2 s "Done Pulling"
-oscsend localhost 4001 /oled/aux/line/3 s "Updates"
+if [ "$changed" -eq 0 ];then
+then
+  oscsend localhost 4001 /oled/aux/line/2 s "No Updates"
+  oscsend localhost 4001 /oled/aux/line/3 s "Pulled"
+else
+  oscsend localhost 4001 /oled/aux/line/2 s "Updates"
+  oscsend localhost 4001 /oled/aux/line/3 s "Pulled"
+fi
+
+
+
+
+
 
